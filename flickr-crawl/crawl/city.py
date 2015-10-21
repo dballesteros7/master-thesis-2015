5294dcb410e104f903eb94ed9c0e54ff19be6ce0
dballesteros7 <diegob@student.ethz.ch>
2015-10-21 19:41:32 +0200

    Load EVERYTHING

--- flickr-crawl/crawl/city.py
+++ flickr-crawl/crawl/city.py
@@ -6,6 +6,8 @@ from crawl.boundingbox import BoundingBox
 class City(Enum):
     LONDON = ('london', BoundingBox(51.672343, 0.148271, 51.384940, -0.351468))
     ZURICH = ('zurich', BoundingBox(47.434680, 8.625370, 47.320230, 8.448060))
+    NEW_YORK = ('new-york', BoundingBox(40.915256, -73.700272, 40.491370, -74.259090))
+    SHANGHAI = ('shanghai', BoundingBox(31.868217, 122.247066, 30.680270, 120.858217))
 
     def __init__(self, city_name: str, bounding_box: BoundingBox):
         self.city_name = city_name
--- flickr-crawl/crawl/flickrservice.py
+++ flickr-crawl/crawl/flickrservice.py
@@ -1,7 +1,8 @@
 import calendar
-import json
 import datetime
+import json
 import logging
+import os
 import time
 
 import flickrapi
@@ -10,50 +11,64 @@ from crawl import secrets
 from crawl.boundingbox import BoundingBox
 from crawl.city import City
 
+_REQUESTS_PER_HOUR_LIMIT = 3000
+
+
+class RateLimiter:
+    def __init__(self):
+        self.requests_in_last_hour = 0
+        self.current_hour = datetime.datetime.now().hour
+
+    def check_and_sleep(self):
+        current_hour = datetime.datetime.now().hour
+        if current_hour != self.current_hour:
+            self.current_hour = current_hour
+            self.requests_in_last_hour = 0
+            logging.info('It is a new hour, resetting rate limiter\'s counter.')
+        elif self.requests_in_last_hour > _REQUESTS_PER_HOUR_LIMIT:
+            nap_time = time.sleep((60 - datetime.datetime.now().minute) * 60)
+            logging.info('Sleeping for {} seconds before continuing, limit has been hit.'.format(nap_time))
+            time.sleep(nap_time)
+        self.requests_in_last_hour += 1
+
 
 class FlickrService:
     _CITY_STREET_ACCURACY = 16
     _PHOTOS_CONTENT_TYPE = 1
-    _REQUESTS_PER_HOUR_LIMIT = 3600
-    _HOUR_IN_SECONDS = 3900
+
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(FlickrService, cls).__new__(cls)
+        return cls._instance
 
     def __init__(self):
-        self.requests_in_last_hour = None
-        self.last_hour_start = None
-        self.flickr = flickrapi.FlickrAPI(secrets.FLICKR_API_KEY, secrets.FLICKR_API_SECRET, format='parsed-json')
+        try:
+            self._initialized
+        except AttributeError:
+            self._initialized = True
+            self._rate_limiter = RateLimiter()
+            self._flickr = flickrapi.FlickrAPI(secrets.FLICKR_API_KEY, secrets.FLICKR_API_SECRET, format='parsed-json')
 
-    def get_geotagged_photos(self, bbox: BoundingBox, start_date: datetime.date, end_date: datetime.date, page: int):
-        if self.last_hour_start is None:
-            self.last_hour_start = time.time()
-            self.requests_in_last_hour = 0
-            logging.info('First API call.')
-        elif time.time() - self.last_hour_start > self._HOUR_IN_SECONDS:
-            self.last_hour_start = time.time()
-            self.requests_in_last_hour = 0
-            logging.info('It has been more than an hour since the first API call in this cycle.')
-        elif self.requests_in_last_hour >= self._REQUESTS_PER_HOUR_LIMIT:
-            sleep_time = self._HOUR_IN_SECONDS - (time.time() - self.last_hour_start)
-            logging.info('Too many calls in an hour, sleeping for {} seconds.'.format(sleep_time))
-            time.sleep(sleep_time)
-            logging.info('Sleeping finished, now continuing.')
-            self.last_hour_start = time.time()
-            self.requests_in_last_hour = 0
-
-        result = self.flickr.photos.search(
+    def get_geotagged_photos(self, bbox: BoundingBox, start_date: datetime.date, end_date: datetime.date,
+                             page: int) -> dict:
+        self._rate_limiter.check_and_sleep()
+        result = self._flickr.photos.search(
             min_taken_date=calendar.timegm(start_date.timetuple()),
             max_taken_date=calendar.timegm(end_date.timetuple()),
             bbox=bbox.to_flickr_bounding_box(),
             accuracy=self._CITY_STREET_ACCURACY, content_type=self._PHOTOS_CONTENT_TYPE, has_geo="1",
-            page=page)
-        self.requests_in_last_hour += 1
-        logging.info('Made another successful API call.')
+            page=page, extras='description,license,date_upload,date_taken,geo,tags,machine_tags,views')
+        logging.info('Got a new result for page {} out of {} with {} photos.'.format(
+            page, result['photos']['pages'], result['photos']['perpage']))
         return result
 
 
-def main(city: City):
+def get_photos_for_city(city: City):
     service = FlickrService()
-    years = range(2011, 2012)
-    output_file_template = '/local/workspace/master-thesis-2015/data/{city_name}-{year}-{page}.json'
+    years = range(2015, 2016)
+    output_file_template = '/local/workspace/master-thesis-2015/data/raw/{city_name}-{year}-{page}.json'
     logging.info('Processing city {}.'.format(city.city_name))
     for year in years:
         logging.info('Getting photos for year {}.'.format(year))
@@ -62,15 +77,15 @@ def main(city: City):
                                               page)
         total_pages = result['photos']['pages']
         for page in range(1, total_pages + 1):
-            logging.info('Getting photos for page {} in year {}.'.format(page, year))
             result = service.get_geotagged_photos(city.bounding_box, datetime.date(year, 1, 1),
                                                   datetime.date(year, 12, 31),
                                                   page)
-            with open(output_file_template.format(city_name=city.city_name, year=year, page=page), 'w') as output_file:
+            output_filename = output_file_template.format(city_name=city.city_name, year=year, page=page)
+            with open(output_filename, 'w') as output_file:
                 json.dump(result['photos']['photo'], output_file)
-                logging.info('Stored file for page {} in year {}.'.format(page, year))
+                logging.info('Stored file {}.'.format(output_filename))
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    main(City.LONDON)
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(funcName)s:%(module)s:%(message)s')
+    get_photos_for_city(City.ZURICH)
